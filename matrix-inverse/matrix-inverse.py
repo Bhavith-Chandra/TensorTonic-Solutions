3b918ec1d73f3166a0e2cb494d2ee513c8a7ce8b
BHAVITHCHANDRA <97584643+Bhavith-Chandra@users.noreply.github.com>
2026-03-10 18:45:48 -0400

    Add solution for matrix-inverse

---
+++ matrix-inverse/matrix-inverse.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def matrix_inverse(A):
+    """
+    Returns: A_inv of shape (n, n) such that A @ A_inv ≈ I
+    """
+    A = np.asarray(A, dtype=float)
+    if A.ndim != 2:
+        return None
+    N,M = A.shape
+    if N != M:
+        return None
+
+    det_A= np.linalg.det(A)
+
+    if abs(det_A) < 1e-10 :
+        return None
+
+    A_inv = np.linalg.inv(A)
+
+    return A_inv
+    # Write code here
+    pass
